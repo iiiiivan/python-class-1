4499201efdaef6d7877b2a7dc5f157d59a1a5dfe
unknown <ivan.chen.0127@gmail.com>
2022-02-19 19:58:46 +0800

    L1

--- L1/flappy/l1.py
+++ L1/flappy/l1.py
@@ -1,3 +1,4 @@
+from typing import List
 from pycat.core import Window, Sprite, Scheduler, KeyCode
 from random import randint
 
@@ -7,6 +8,9 @@ w = Window(background_image='background.png',
             enforce_window_limits=False)
 
 PIPE_GAP=0.4*w.height
+GRAVITY=-0.2
+PIPE_SPEED=5
+FLAP_SPEED=4
 
 
 class Pipe(Sprite):
@@ -14,9 +18,11 @@ class Pipe(Sprite):
         self.image='pipe.png'
         self.scale*=0.65
         self.x=w.width+self.width/2
+        self.add_tag('pipe')
+
 
     def on_update(self, dt):
-        self.x-=5
+        self.x-=PIPE_SPEED
         if self.x<-self.width/2:
             self.delete()
 
@@ -30,7 +36,13 @@ def create_pipe(dt):
     p2.y=gap_y + PIPE_GAP/2 + p2.height/2
     p2.rotation=180
 
-GRAVITY=-0.2
+    if randint(1, 4)==1:
+        b=w.create_sprite(Bomb)
+        b.x=p1.x
+        b.y=gap_y
+        b.pipes=[p1, p2] 
+
+
 
 class Bird(Sprite):
     def on_create(self):
@@ -45,8 +57,32 @@ class Bird(Sprite):
         self.y_speed+=GRAVITY
 
         if w.is_key_down(KeyCode.SPACE):
-            self.y_speed=2
-  
+            self.y_speed=4
+
+        if self.is_touching_any_sprite_with_tag('pipe'):
+            w.close()
+        
+        if self.y<0:
+            w.close()
+
+class Bomb(Sprite):
+    def on_create(self):
+        self.image='bomb.png'
+        self.scale=0.1
+        self.pipes:List[Pipe]=[]
+
+    def on_update(self, dt):
+        self.x-=PIPE_SPEED
+
+    def on_left_click(self):
+        self.image='boom.png'
+        for p in self.pipes:
+            p.delete()
+        
+        if self.x<-self.width/2:
+            self.delete()
+
+
 Scheduler.update(create_pipe, 1.5)
 create_pipe(0)
 w.create_sprite(Bird)
