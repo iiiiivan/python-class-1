de740ea8c78631dc9ad2982b5d138ff8c60242f7
ivan <ivan.chen.0127@gmail.com>
2022-05-28 20:04:47 +0800

    L10

--- L10/l10.py
+++ L10/l10.py
@@ -4,10 +4,26 @@ from pycat.core import Window, Sprite, Scheduler, Label
 
 w=Window()
 
-games=['minecraft', 'war games', 'spider man']
-sports=['running', 'swimming', 'table tennies']
-languages=['Chinese', 'English', 'Japanese']
+def get_text_list(file_path: str):
+    result = []
+    with open(file_path, 'r') as file:
+        for line in file.readlines():
+            result.append(line.strip())
+    return result
 
+games = get_text_list('L10/games.txt')
+languages = get_text_list('L10/languages.txt')
+sports = get_text_list('L10/sports.txt')
+
+
+class HighScore(Label):
+    def on_create(self):
+        with open('L10/high_score.txt', 'r') as file:
+            self.high_score = int(file.readline())
+        self.text = 'High Score: ' + str(self.high_score)
+        self.x = w.center.x
+        
+high_score_label = w.create_label(HighScore)
 
 class WordGroup(Enum):
     GAMES=auto()
@@ -37,9 +53,9 @@ class Word(Sprite):
         self.height=self.label.content_height
 
     def update_lable_position(self):
-        self.label.position=self.position
-        self.label.x-=self.label.content_width/2
-        self.label.y+=self.label.content_height/2
+        self.label.position = self.position
+        self.label.x -=self.label.content_width/2
+        self.label.y +=self.label.content_height/2
 
     def delete(self):
         super().delete()
@@ -82,6 +98,12 @@ class Score(Label):
     def score(self, value):
         self.__score=value
         self.text='score: ' + str(self.score)
+        if value > high_score_label.high_score:
+            
+            high_score_label.high_score = value
+            with open('L10/high_score.txt', 'w') as file:
+                file.write(str(high_score_label.high_score))
+            high_score_label.text = 'High Score: ' + str(high_score_label.high_score)
 
 
 group_label=w.create_label(text=str(Word.group))    
